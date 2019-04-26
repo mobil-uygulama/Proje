181bdacccc4b219d98fca8c8cc8cb967471a8c52
SerkanSahin11 <serkan114sahin@gmail.com>
2019-04-26 22:16:08 +0300

    firebase ayarları eklendi

---
+++ src/app/firebaseconfig.ts
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyDfFkUjORRnzvWbjPebZJ1ESNtKq8IxTLE",
+  authDomain: "mobiluygulama-bilisim.firebaseapp.com",
+  databaseURL: "https://mobiluygulama-bilisim.firebaseio.com",
+  projectId: "mobiluygulama-bilisim",
+  storageBucket: "mobiluygulama-bilisim.appspot.com",
+  messagingSenderId: "1066319127530"
+  };
+
+  export default config
