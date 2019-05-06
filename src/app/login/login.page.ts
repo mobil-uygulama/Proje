1c5d7d97cbf099699213408c68ca13d4d0361d53
SerkanSahin11 <serkan114sahin@gmail.com>
2019-05-06 18:34:04 +0300

    Tasarım Düzenlemeleri

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth'
 import {auth} from 'firebase/app'
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 
 
 @Component({
