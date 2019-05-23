b4dbbcba098ae9731706897e42b5508b50e1420a
mynameisPadna <emirhan20aydin@gmail.com>
2019-05-24 01:49:57 +0300

    TakvimYapıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import {NgCalendarModule} from 'ionic2-calendar'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import firebaseConfig from './firebaseconfig'
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireAuthModule} from '@angular/fire/auth'
+import { AngularFirestoreModule} from '@angular/fire/firestore';
+import { initializeApp } from 'firebase';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,6 +20,9 @@ import {AngularFireAuthModule} from '@angular/fire/auth'
     IonicModule.forRoot(), 
     AppRoutingModule,
     AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    NgCalendarModule,
+    AngularFirestoreModule,
     AngularFireAuthModule
   ],
   providers: [
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AlertController } from '@ionic/angular';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  eventSource=[];
+  viewTitle;
+  nameevent : string = ""
+
+  constructor(private db: AngularFirestore,private alertcontroller:AlertController) {
+    this.db.collection(`events`).snapshotChanges().subscribe(colSnap => {
+      this.eventSource = [];
+      colSnap.forEach(snap => {
+        let event:any = snap.payload.doc.data();
+        event.id = snap.payload.doc.id;
+        event.startTime = event.startTime.toDate();
+        event.endTime = event.endTime.toDate();
+        console.log(event);
+        this.eventSource.push(event);
+      });
+    });
+  }
+
+  
+
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+  };
+  selectedDate = new Date();
 
   ngOnInit() {
   }
+
+  onViewTitleChanged(title) {
+    console.log(title);
+    this.viewTitle=title;
+  }
+
+  onEventSelected(event) {
+    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+  }
+
+  onTimeSelected(ev) {
+    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    this.selectedDate = ev.selectedTime;
+  }
+
+  onCurrentDateChanged(event: Date) {
+    console.log('current date change: ' + event);
+  }
+
+  onRangeChanged(ev) {
+    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+  }
+
+  async presentAlert() {
+    const alert = await this.alertcontroller.create({
+      header: 'Alert',
+      message: 'Event Added',
+      buttons: ['OK']
+    });
+    await alert.present();
+    
+  }
+
+
+  addNewEvent() {
+    let start = this.selectedDate;
+    let end = this.selectedDate;
+    end.setMinutes(end.getMinutes() + 60);
+
+    let event = {
+      title: this.nameevent,
+      startTime: start,
+      endTime: end,
+      allDay: false,
+    };
+    this.db.collection(`events`).add(event);
+    this.presentAlert()
+  }
+
+  changeMode(mode) {
+    this.calendar.mode = mode;
+}
 
 }
