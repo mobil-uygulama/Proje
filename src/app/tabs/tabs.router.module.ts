01e3a003ce55410a93b856341658095be7452855
mynameisPadna <emirhan20aydin@gmail.com>
2019-05-30 19:18:06 +0300

    Düzenlemeler Yapıldı

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
 			{ path: 'profile', loadChildren: '../profile/profile.module#ProfilePageModule' },
 			{ path: 'post/:id', loadChildren: '../post/post.module#PostPageModule' },
 			{ path: 'edit-profile', loadChildren: '../edit-profile/edit-profile.module#EditProfilePageModule' },
+			{ path: 'calendar', loadChildren: '../home/home.module#HomePageModule' }
 		]
 	}	
 ];
